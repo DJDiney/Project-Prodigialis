b6648c48468d95f305161ef55731f4bc4050e11e
Felipe Rabelo <carbonemasseo@hotmail.com>
2016-10-30 18:27:25 -0200

    Mudanças nas interfaces

--- prodigialis_core/src/br/cefetmg/inf/prodigialis/model/dao/IProcessoSeletivoDAO.java
+++ prodigialis_core/src/br/cefetmg/inf/prodigialis/model/dao/IProcessoSeletivoDAO.java
@@ -1,20 +1,12 @@
 
 package br.cefetmg.inf.prodigialis.model.dao;
 
-import br.cefetmg.inf.prodigialis.model.domain.ProcessoSeletivo;
+import br.cefetmg.inf.prodigialis.model.domain.Participante;
 import br.cefetmg.inf.prodigialis.util.db.exception.PersistenciaException;
-import java.util.List;
 
 
 public interface IProcessoSeletivoDAO {
-
-    public boolean inserir(ProcessoSeletivo obj) throws PersistenciaException;
-    public boolean atualizar(ProcessoSeletivo obj, String antigo) throws PersistenciaException;
-    public boolean excluir(String nome) throws PersistenciaException;
-    public ProcessoSeletivo consultarPorId(Long id) throws PersistenciaException;
-    public List<ProcessoSeletivo> listarTodos() throws PersistenciaException;
-    public ProcessoSeletivo consultarPorNome(String nome) throws PersistenciaException;
-    public List<ProcessoSeletivo> consultarPorNomeDistinto() throws PersistenciaException;
-    public List<ProcessoSeletivo> consultarPorNomeDistintoUnico() throws PersistenciaException;
+    
+    public boolean inserir(Participante obj) throws PersistenciaException;
     
 }
