d1c89305ae5e6911f02f87fd2b11a80d2c98e483
Felipe Rabelo <carbonemasseo@hotmail.com>
2016-11-03 10:26:17 -0200

    Adicionado novo método

--- prodigialis_core/src/br/cefetmg/inf/prodigialis/model/dao/IProvaDAO.java
+++ prodigialis_core/src/br/cefetmg/inf/prodigialis/model/dao/IProvaDAO.java
@@ -1,8 +1,15 @@
 
 package br.cefetmg.inf.prodigialis.model.dao;
 
+import br.cefetmg.inf.prodigialis.model.domain.Participante;
 import br.cefetmg.inf.prodigialis.model.domain.Prova;
+import br.cefetmg.inf.prodigialis.util.db.exception.PersistenciaException;
 import br.cefetmg.inf.prodigialis.util.db.persistence.GenericDAO;
+import java.util.List;
 
 
-public interface IProvaDAO extends GenericDAO<Prova>{}
+public interface IProvaDAO extends GenericDAO<Prova>{
+
+    public List<Participante> listarParticipantes (String cod_prova) throws PersistenciaException;
+
+}
