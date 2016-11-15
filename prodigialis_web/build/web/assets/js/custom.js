04ddcb35c41c43fd5f6a16b7d3d9f7b5e1812000
Eike999 <phenrique.lacerda@hotmail.com>
2016-11-15 19:37:55 -0200

    qe ta com teseno

--- prodigialis_web/build/web/assets/js/custom.js
+++ prodigialis_web/build/web/assets/js/custom.js
@@ -1,2 +1,27 @@
-
-
+$(document).ready(function () {
+	$('#ArquivoProva').change(function () {
+		var filename = $('#ArquivoProva').val();
+		$('#DirArquivoProva').html(filename);
+	});
+	
+	$('#ArquivoGabarito').change(function () {
+		var filename = $('#ArquivoGabarito').val();
+		$('#DirArquivoGabarito').html(filename);
+	});
+	
+	$('#AddSpecs').click(function(){
+		var spec = $('#NewSpec').val();
+		if(spec == '' || spec == null || spec == ' '){}
+		else{
+			$("#SpecsList").append('<li class="list-group-item">'+spec+'<button  class="btn btn-danger btn-circle btn-simple pull-right btn-deleter"><i class="fa fa-times" style="vertical-align:center"></i></button></li>');
+		}
+	});
+	
+	$('#SpecsList').on('click', '.btn-deleter', function() {
+		$(this).parent().remove();
+	});
+	$('#file-7').change(function () {
+		var filename = $('#file-7').val();
+		$('#Filename').html(filename);
+	});
+});
