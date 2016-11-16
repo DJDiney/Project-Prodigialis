55603a2225efefa1569727a1d87c344de5809145
Eike999 <phenrique.lacerda@hotmail.com>
2016-11-16 20:48:56 -0200

    Revert "CPF"
    This reverts commit a08e664988c45de12b12418c928f6e8b88026fa4.

--- prodigialis_web/web/x_login-register modal/login-register.js
+++ prodigialis_web/web/x_login-register modal/login-register.js
@@ -57,10 +57,10 @@ function loginAjax(){
 
 /*   Simulate error message from the server   */
 	if(document.getElementById("email").value=="admin"){
-		window.location.href = "MenuFunc(Video).html"
+		window.location.href = "MenuFunc.html"
 	}
-	else if(document.getElementById("email").value=="user"){
-		window.location.href = "MenuUser(Video).html"
+	if(document.getElementById("email").value=="user"){
+		window.location.href = "MenuUser.html"
 	}
 	else{
      shakeModal();
