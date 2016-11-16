c797e71af911ef3aa884beb658582d88689ff140
Felipe Rabelo <carbonemasseo@hotmail.com>
2016-11-15 23:21:49 -0200

    ManterProcesso
    NÃO ESQUECE DE OLHAR PRA VER SE TE AGRADA, essa é a parte q eu n tenho certeza se tá tudo certo. Pq aqui se válida a informação digitada pelo usuario, n sei se vc concorda com os valores obrigatorios que eu coloquei.

---
+++ prodigialis_core/src/br/cefetmg/inf/prodigialis/model/service/impl/ManterProcessoSeletivo.java
@@ -0,0 +1,139 @@
+package br.cefetmg.inf.prodigialis.model.service.impl;
+
+import br.cefetmg.inf.prodigialis.model.dao.IParticipanteDAO;
+import br.cefetmg.inf.prodigialis.model.dao.IProcessoSeletivoDAO;
+import br.cefetmg.inf.prodigialis.model.dao.impl.ParticipanteDAO;
+import br.cefetmg.inf.prodigialis.model.dao.impl.ProcessoSeletivoDAO;
+import br.cefetmg.inf.prodigialis.model.domain.Participante;
+import br.cefetmg.inf.prodigialis.model.domain.ProcessoSeletivo;
+import br.cefetmg.inf.prodigialis.model.service.IManterProcessoSeletivo;
+import br.cefetmg.inf.prodigialis.util.db.exception.NegocioException;
+import br.cefetmg.inf.prodigialis.util.db.exception.PersistenciaException;
+import java.util.List;
+
+/**
+ *
+ * @author Felipe Rabelo
+ */
+public class ManterProcessoSeletivo implements IManterProcessoSeletivo {
+    
+    private IProcessoSeletivoDAO processoDAO;
+
+    public ManterProcessoSeletivo() {
+        
+        processoDAO = new ProcessoSeletivoDAO();
+        
+    }
+
+    @Override
+    public boolean inserir(ProcessoSeletivo obj) throws PersistenciaException, NegocioException {
+        
+        if (obj == null)
+            
+            throw new NegocioException("Dados do processo não foram informados.");
+        
+        if (obj.getDataInicio() == null)
+            
+            throw new NegocioException("Data do processo deve ser informada.");
+        
+        if (obj.getDataFinal() == null)
+            
+            throw new NegocioException("Data do fim do processo deve ser informada.");
+        
+        if (obj.getNome() == null)
+            
+            throw new NegocioException("Nome do processo deve ser informado.");
+        
+        if (obj.getNroVagas() == null)
+            
+            throw new NegocioException("Número de vagas do processo deve ser informado.");
+        
+        if (obj.getCargoOferecido() == null)
+            
+            throw new NegocioException("Cargo concorrido deve ser informado.");
+        
+        boolean processoCod = processoDAO.inserir(obj);
+        
+        return processoCod;
+        
+    }
+
+    @Override
+    public boolean atualizar(ProcessoSeletivo obj) throws PersistenciaException, NegocioException {
+        
+        if (obj == null)
+            
+            throw new NegocioException("Dados do processo não foram informados.");
+        
+        if (obj.getCodProcesso() == null) 
+            
+            throw new NegocioException("Codigo da processo não foi informado.");
+        
+        if (obj.getProva() == null)
+            
+            throw new NegocioException("Codigo da prova não foi informado.");
+        
+        if (obj.getDataInicio() == null)
+            
+            throw new NegocioException("Data do processo deve ser informada.");
+        
+        if (obj.getDataFinal() == null)
+            
+            throw new NegocioException("Data do fim do processo deve ser informada.");
+        
+        if (obj.getNome() == null)
+            
+            throw new NegocioException("Nome do processo deve ser informado.");
+        
+        if (obj.getDescricao() == null) 
+            
+            throw new NegocioException("Descrição do processo não informada.");
+        
+        if (obj.getNroVagas() == null)
+            
+            throw new NegocioException("Número de vagas do processo deve ser informado.");
+        
+        if (obj.getCargoOferecido() == null)
+            
+            throw new NegocioException("Cargo concorrido deve ser informado.");
+        
+        boolean result = processoDAO.atualizar(obj);
+        
+        return result;
+        
+    }
+
+    @Override
+    public boolean excluir(Long id) throws PersistenciaException {
+        
+        boolean result = processoDAO.excluir(id);
+        return result; 
+        
+    }
+
+    @Override
+    public Participante consultarPorId(Long id) throws PersistenciaException {
+        
+        IParticipanteDAO participante = new ParticipanteDAO();
+        Participante obj = participante.consultarPorId(id);
+        return obj;
+        
+    }
+
+    @Override
+    public List<ProcessoSeletivo> listarTodos() throws PersistenciaException {
+        
+        List<ProcessoSeletivo> listProcesso = processoDAO.listarTodos();
+        return listProcesso;
+        
+    }
+
+    @Override
+    public ProcessoSeletivo consultarProcesssoPorId(Long id) throws PersistenciaException {
+        
+        ProcessoSeletivo result = processoDAO.consultarPorId(id);
+        return result; 
+        
+    }
+    
+}
